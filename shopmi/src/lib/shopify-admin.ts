e9f03884c299c262d900aa7d5d4be31bd04dd569
gubressan <gustavobressanin6@gmail.com>
2025-05-06 14:27:01 -0300

    fix elint2

--- shopmi/src/lib/shopify-admin.ts
+++ shopmi/src/lib/shopify-admin.ts
@@ -293,7 +293,8 @@ export const adminOperations = {
            if (!(edge && typeof edge === 'object' && 'node' in edge && edge.node && typeof edge.node === 'object')) {
              return null; // Ou alguma outra forma de tratamento de erro/valor padrão
            }
-           const node = edge.node as { title?: string, subcollectionsMetafield?: { value?: string }, [key: string]: any }; // Type assertion mais específica
+           // eslint-disable-next-line @typescript-eslint/no-explicit-any
+           const node = edge.node as { title?: string, subcollectionsMetafield?: { value?: string }, [key: string]: unknown }; // Alterado para unknown
            let subcollections = [];
 
           if (node.subcollectionsMetafield && node.subcollectionsMetafield.value) {
